25a8a830fdbb720c71f1ecad4350275e17cacf71
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2026-03-02 16:37:33 +0530

    Set up Flask application with a home route

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("This is Jenkins-Prac")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Jenkins CI/CD 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
